c591d075f742b3484bff00c1f65afca53aed70e7
Hawwa11 <aliyanajackson39@gmail.com>
2020-12-30 14:35:25 +0800

    commit 3

--- apps/study_app/lib/main.dart
+++ apps/study_app/lib/main.dart
@@ -1,7 +1,34 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp();
-  // studying on how each and every widgets works 
+void main() {//widget Tree
+  runApp(MaterialApp(
+    home:Scaffold(//Its a wrapper
+      appBar:AppBar(
+        title:Text('Raynu first App'),
+        centerTitle: true,//this is to make the title in the middle !
+     backgroundColor:Colors.lime,
+      ),
+body: Center(
+  //centralized whatever nested inside of it !!
+  child: Text(
+    'hello everyone!',
+    style:TextStyle(
+      fontSize: 20.0,
+ ),
+  ),
+
+      ),
+   floatingActionButton: FloatingActionButton(
+     onPressed: (){},
+   child: Text('Click'),
+     backgroundColor: Colors.lime,
+),
+    ),
+  ));
+  //Scalffold -> basic layout of the app !
+  //we can create our own customize widgets!
+  // studying on how each and every widgets works
+
+
 }
 
